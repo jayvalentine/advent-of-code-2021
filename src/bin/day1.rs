b87130f77aeaddce4675e8b4177aecad0d35de32
Jay Valentine <jayv136@gmail.com>
2021-12-02 12:30:48 +0000

    Day 1: Adding example for part 1 as test

---
+++ src/bin/day1.rs
@@ -0,0 +1,33 @@
+// Advent of Code 2021
+// Day 1
+
+#[cfg(test)]
+mod test_examples {
+    use super::*;
+
+    #[test]
+    fn part1() {
+        let input = vec![
+            199,
+            200,
+            208,
+            210,
+            200,
+            207,
+            240,
+            269,
+            260,
+            263,
+        ];
+
+        assert_eq!(7, num_decreases(&input));
+    }
+}
+
+fn num_decreases(report: &[u32]) -> u32 {
+    return 7;
+}
+
+fn main() {
+
+}
