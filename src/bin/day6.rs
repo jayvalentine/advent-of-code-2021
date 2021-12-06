7f69469d3061f1f79335ea054e5f9d82877a15b9
Jay Valentine <jayv136@gmail.com>
2021-12-06 17:21:12 +0000

    Day 6: Fish with timers are bad

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -22,6 +22,13 @@ mod test_examples {
         assert_eq!(26, lanternfish_pop(&lanternfish, 18));
         assert_eq!(5934, lanternfish_pop(&lanternfish, 80));
     }
+
+    #[test]
+    fn part2() {
+        let lanternfish = vec![3, 4, 3, 1, 2];
+
+        assert_eq!(26984457539, lanternfish_pop(&lanternfish, 256));
+    }
 }
 
 #[cfg(test)]
@@ -56,6 +63,62 @@ mod test_lanternfish {
     }
 }
 
+#[cfg(test)]
+mod test_tick {
+    use super::*;
+
+    #[test]
+    fn next_pop_1() {
+        let p = vec![0, 2, 0, 0, 0, 0, 0, 0, 0];
+        let p2 = next_pop(p);
+        assert_eq!(9, p2.len());
+        assert_eq!(2, p2[0]);
+        assert_eq!(0, p2[1]);
+    }
+
+    #[test]
+    fn next_pop_0() {
+        let p = vec![2, 0, 0, 0, 0, 0, 0, 0, 0];
+        let p2 = next_pop(p);
+        assert_eq!(9, p2.len());
+        assert_eq!(0, p2[0]);
+        assert_eq!(2, p2[6]);
+        assert_eq!(2, p2[8]);
+    }
+
+    #[test]
+    fn next_pop_0_1_4() {
+        let p = vec![2, 3, 0, 0, 5, 0, 0, 0, 0];
+        let p2 = next_pop(p);
+        assert_eq!(9, p2.len());
+        assert_eq!(3, p2[0]);
+        assert_eq!(0, p2[1]);
+        assert_eq!(0, p2[2]);
+        assert_eq!(5, p2[3]);
+        assert_eq!(0, p2[4]);
+        assert_eq!(0, p2[5]);
+        assert_eq!(2, p2[6]);
+        assert_eq!(0, p2[7]);
+        assert_eq!(2, p2[8]);
+    }
+
+    #[test]
+    fn next_pop_all() {
+        let p = vec![2, 3, 2, 1, 4, 5, 3, 2, 1];
+        let p2 = next_pop(p);
+        assert_eq!(9, p2.len());
+        assert_eq!(3, p2[0]);
+        assert_eq!(2, p2[1]);
+        assert_eq!(1, p2[2]);
+        assert_eq!(4, p2[3]);
+        assert_eq!(5, p2[4]);
+        assert_eq!(3, p2[5]);
+        assert_eq!(4, p2[6]);
+        assert_eq!(1, p2[7]);
+        assert_eq!(2, p2[8]);
+    }
+}
+
 struct Lanternfish {
     timer: u32
 }
@@ -73,27 +136,39 @@ impl Lanternfish {
     }
 }
 
+fn next_pop(pop: Vec<u64>) -> Vec<u64> {
+    // Sanity check - shouldn't ever change vector size.
+    if pop.len() != 9 {
+        panic!("Vector of wrong size!");
+    }
+
+    let mut new_pop = vec![0; 9];
+
+    let reproducing = pop[0];
+
+    for p in 0..8 {
+        new_pop[p] = pop[p+1];
+    }
+
+    // Those that have reproduced tick over to 6,
+    // and produce new fish with timer 8.
+    new_pop[6] += reproducing;
+    new_pop[8] += reproducing;
+
+    return new_pop;
+}
+
 fn lanternfish_pop(initial_pop: &[u32], days: u32) -> usize {
-    let mut lanternfish: Vec<Lanternfish> = Vec::new();
-    for n in initial_pop {
-        lanternfish.push(Lanternfish { timer: *n });
+    let mut population = vec![0; 9];
+    for &p in initial_pop {
+        population[p as usize] += 1;
     }
 
     for _ in 0..days {
-        let mut new_lanternfish = Vec::new();
-        for l in &mut lanternfish {
-            let s = l.tick();
-            if s.is_some() {
-                new_lanternfish.push(s.unwrap());
-            }
-        }
-
-        for n in new_lanternfish {
-            lanternfish.push(Lanternfish { timer: n })
-        }
+        population = next_pop(population);
     }
 
-    return lanternfish.len();
+    return population.iter().fold(0, |acc, x| acc + x) as usize;
 }
 
 fn part1() -> usize {
@@ -101,7 +176,15 @@ fn part1() -> usize {
     return lanternfish_pop(&initial_pop, 80);
 }
 
+fn part2() -> usize {
+    let initial_pop = data::get("data/day6.txt");
+    return lanternfish_pop(&initial_pop, 256);
+}
+
 fn main() {
     let final_pop = part1();
     println!("Final lanternfish population after 80 days is {}", final_pop);
+
+    let final_pop = part2();
+    println!("Final lanternfish population after 256 days is {}", final_pop);
 }
