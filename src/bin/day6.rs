5b883aa31c8d3900b3f2171125f699ab288a4502
Jay Valentine <jayv136@gmail.com>
2021-12-06 11:22:17 +0000

    Day 6: Implementing lanternfish logic

---
+++ src/bin/day6.rs
@@ -0,0 +1,77 @@
+// Advent of Code 2021
+// Day 6
+
+#[cfg(test)]
+mod test_examples {
+    use super::*;
+
+    #[test]
+    fn part1() {
+        let lanternfish = vec![3, 4, 3, 1, 2];
+
+        assert_eq!(26, lanternfish_pop(&lanternfish, 18));
+        assert_eq!(5934, lanternfish_pop(&lanternfish, 80));
+    }
+}
+
+#[cfg(test)]
+mod test_lanternfish {
+    use super::*;
+
+    #[test]
+    fn test_tick() {
+        let mut l = Lanternfish { timer: 3 };
+        let s = l.tick();
+
+        assert_eq!(None, s);
+        assert_eq!(2, l.timer);
+    }
+
+    #[test]
+    fn test_tick_0() {
+        let mut l = Lanternfish { timer: 0 };
+        let s = l.tick();
+
+        assert_eq!(Some(8), s);
+        assert_eq!(6, l.timer);
+    }
+
+    #[test]
+    fn test_tick_1() {
+        let mut l = Lanternfish { timer: 1 };
+        let s = l.tick();
+
+        assert_eq!(None, s);
+        assert_eq!(0, l.timer);
+    }
+}
+
+struct Lanternfish {
+    timer: u32
+}
+
+impl Lanternfish {
+    fn tick(&mut self) -> Option<u32> {
+        if self.timer == 0 {
+            self.timer = 6;
+            return Some(8);
+        }
+        else {
+            self.timer -= 1;
+            return None;
+        }
+    }
+}
+
+fn lanternfish_pop(initial_pop: &[u32], days: u32) -> u32 {
+    let mut lanternfish: Vec<Lanternfish> = Vec::new();
+    for n in initial_pop {
+        lanternfish.push(Lanternfish { timer: *n });
+    }
+
+    return 0;
+}
+
+fn main() {
+
+}
